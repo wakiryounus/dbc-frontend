9b46f94385e9822d849c7a4167a3e73cb2a392fc
wakiryounus <wakiryounus@gmail.com>
2024-10-21 12:44:43 +0530

    used styled for typography

--- src/components/dbc-typography.tsx
+++ src/components/dbc-typography.tsx
@@ -1,31 +1,24 @@
 import React from "react";
 import { TypographyProps } from "@mui/material/Typography/Typography";
-import { Theme, Typography } from "@mui/material";
-import { createStyles, makeStyles } from "@mui/styles";
-import clsx from "clsx";
+import { Typography, useTheme } from "@mui/material";
 
 interface DbcTypographyProps extends TypographyProps {
   light?: boolean;
 }
-type StyleProps = {
-  light?: boolean;
-};
-
-export const useStyles = makeStyles<Theme, StyleProps>((theme: Theme) =>
-  createStyles({
-    root: {
-      color: ({ light }) =>
-        light ? theme.palette.primary.main : theme.palette.secondary.main,
-    },
-  }),
-);
 
 const DbcTypography: React.FC<DbcTypographyProps> = (props) => {
-  const { light, className, children, ...rest } = props;
-  const classes = useStyles({ light });
+  const { light, children, ...rest } = props;
+  const theme = useTheme();
 
   return (
-    <Typography {...rest} className={clsx(classes.root, className)}>
+    <Typography
+      sx={{
+        color: light
+          ? theme.palette.primary.main
+          : theme.palette.secondary.main,
+      }}
+      {...rest}
+    >
       {children}
     </Typography>
   );
